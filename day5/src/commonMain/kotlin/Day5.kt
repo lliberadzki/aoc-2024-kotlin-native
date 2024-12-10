3f54cbd99fa8414557ce4ac5228c3aa08504843c
lukas <lukas@Air-lukas.lan>
2024-12-10 01:19:09 +0100

    Day 5

--- day5/src/commonMain/kotlin/Day5.kt
+++ day5/src/commonMain/kotlin/Day5.kt
@@ -4,26 +4,59 @@ import kotlinx.cinterop.*
 import platform.posix.*
 
 fun main() {
-    println(part1())
-    println(part2())
+    println(read())
 }
 
-fun part1(): Any {
+fun read(): Any {
     return input
-}
-
-fun part2(): Any {
-    return input.reversed()
 }
 
 private val input: String by lazy { readInput()}
 
+fun putOrAddtoMap(key: Int, value: Int, map: HashMap<Int, HashSet<Int>>) {
+    if (map.containsKey(key)) {
+        map[key]?.add(value)
+    } else {
+        map[key] = HashSet()
+        map[key]?.add(value)
+    }
+}
+
+fun isGood(pages: List<Int>, beforeMap: HashMap<Int, HashSet<Int>>): Boolean {
+    for (i in 1..<pages.size) {
+        if (beforeMap.containsKey(pages[i]) && beforeMap[pages[i]]?.containsAll(pages.subList(0, i))!!) {
+            // report is good so far
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+class RulesComparator(val beforeMap: HashMap<Int, HashSet<Int>>, val afterMap: HashMap<Int, HashSet<Int>>): Comparator<Int> {
+    override fun compare(p1: Int, p2: Int): Int {
+        if (this.beforeMap.containsKey(p1) && this.beforeMap[p1]?.contains(p2)!!) return 1
+        if (this.afterMap.containsKey(p2) && this.afterMap[p2]?.contains(p1)!!) return -1
+        return 0
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
 private fun readInput(): String {
-    val returnBuffer = StringBuilder()
     val cwd = getcwd(null, 0U)?.toKString()
     val resourceDir = "${cwd}/src/commonMain/resources"
     val file = fopen("$resourceDir/Day5.input", "r") ?:
     throw IllegalArgumentException("Cannot open input file Day5.input")
+
+    val beforeMap = HashMap<Int, HashSet<Int>>()
+    val afterMap = HashMap<Int, HashSet<Int>>()
+    var readingReports = true
+    var pages: List<Int>
+    var sortedPages: List<Int>
+    var acc = 0
+    var beforeVal: Int
+    var afterVal: Int
+    var rulesComparator = RulesComparator(HashMap(), HashMap())
 
     try {
         memScoped {
@@ -31,13 +64,40 @@ private fun readInput(): String {
             val buffer = allocArray<ByteVar>(readBufferLength)
             var line = fgets(buffer, readBufferLength, file)?.toKString()
             while (line != null) {
-                returnBuffer.append(line)
+                // operate on line
+                if (line.trim() == "") {
+                    //println("before map: " + beforeMap)
+                    //println("before map with counts: " + beforeMap.map { Pair(it.key, it.value.size) }.toList())
+                    rulesComparator = RulesComparator(beforeMap, afterMap)
+                    readingReports = false
+                    line = fgets(buffer, readBufferLength, file)?.toKString()
+                    continue
+                }
+
+                if (readingReports) {
+                    beforeVal = line.substring(0, 2).toInt()
+                    afterVal = line.substring(3, 5).toInt()
+                    putOrAddtoMap(afterVal, beforeVal, beforeMap)
+                    putOrAddtoMap(beforeVal, afterVal, afterMap)
+                }
+                if (!readingReports) {
+                    pages = line.trim().split(',').map { it.toInt() }.toList()
+                    if (!isGood(pages, beforeMap)) {
+                        //println(pages)
+                        sortedPages = pages.sortedWith(rulesComparator)
+                        //println(sortedPages)
+                        acc += sortedPages[sortedPages.size / 2]
+                    }
+                }
+
                 line = fgets(buffer, readBufferLength, file)?.toKString()
             }
+
+            println(acc)
         }
     } finally {
         fclose(file)
     }
 
-    return returnBuffer.toString()
+    return ""
 }
