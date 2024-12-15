df790f5431a04b034a0cb45ca1c366d0af86721b
lukas <lukas@Air-lukas.lan>
2024-12-15 21:36:41 +0100

    Day 7

--- day7/src/commonMain/kotlin/Day7.kt
+++ day7/src/commonMain/kotlin/Day7.kt
@@ -4,26 +4,45 @@ import kotlinx.cinterop.*
 import platform.posix.*
 
 fun main() {
-    println(part1())
-    println(part2())
+    println(read())
 }
 
-fun part1(): Any {
-    return input
-}
-
-fun part2(): Any {
+fun read(): Any {
     return input.reversed()
 }
 
-private val input: String by lazy { readInput()}
+private val input: String by lazy { readInput() }
 
+fun concatenate(val1: Long, val2: Long): Long {
+    var xScale = 1
+    while (xScale <= val2) {
+        xScale *= 10
+    }
+    return val1 * xScale + val2
+}
+
+fun testEquation(expected: Long, values: List<Long>, current: Long, index: Int): Boolean {
+    if (index == 0) {
+        return testEquation(expected, values, values[0], 1)
+    }
+    if (index == values.size - 1) {
+        return (expected == current+values[index]) ||
+                (expected == current*values[index]) ||
+                (expected == concatenate(current, values[index]))
+    }
+    return testEquation(expected, values, current+values[index], index+1) ||
+            testEquation(expected, values, current*values[index], index+1) ||
+            testEquation(expected, values, concatenate(current, values[index]), index+1)
+}
+
+@OptIn(ExperimentalForeignApi::class)
 private fun readInput(): String {
-    val returnBuffer = StringBuilder()
     val cwd = getcwd(null, 0U)?.toKString()
     val resourceDir = "${cwd}/src/commonMain/resources"
     val file = fopen("$resourceDir/Day7.input", "r") ?:
     throw IllegalArgumentException("Cannot open input file Day7.input")
+
+    val calibrations = ArrayList<String>()
 
     try {
         memScoped {
@@ -31,13 +50,31 @@ private fun readInput(): String {
             val buffer = allocArray<ByteVar>(readBufferLength)
             var line = fgets(buffer, readBufferLength, file)?.toKString()
             while (line != null) {
-                returnBuffer.append(line)
+                // todo
+                calibrations.add(line.trim())
                 line = fgets(buffer, readBufferLength, file)?.toKString()
             }
         }
+
+        var expected: Long
+        var values: List<Long>
+        var splitResult: List<String>
+        var acc: Long = 0
+
+        for (calibration in calibrations) {
+            splitResult = calibration.split(":", limit = 2)
+            expected = splitResult[0].toLong()
+            values = splitResult[1].trim().split(" ").map { it.toLong() }.toList()
+
+            if (testEquation(expected, values, 0, 0)) {
+                acc += expected
+            }
+        }
+
+        println(acc)
     } finally {
         fclose(file)
     }
 
-    return returnBuffer.toString()
+    return ""
 }
