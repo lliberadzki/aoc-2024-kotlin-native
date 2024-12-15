d2447d14d7854a5a2b95e1f66167368e8b8a5c4b
lukas <lukas@Air-lukas.lan>
2024-12-15 20:01:31 +0100

    Day 6

--- day6/src/commonMain/kotlin/Day6.kt
+++ day6/src/commonMain/kotlin/Day6.kt
@@ -4,26 +4,68 @@ import kotlinx.cinterop.*
 import platform.posix.*
 
 fun main() {
-    println(part1())
-    println(part2())
+    println(read())
 }
 
-fun part1(): Any {
+fun read(): Any {
     return input
 }
 
-fun part2(): Any {
-    return input.reversed()
+private val input: String by lazy { readInput() }
+
+fun printCharArray(puzzle: List<CharArray>) {
+    println("puzzle:")
+    println(puzzle.map { it.joinToString("") }.joinToString("\n"))
+
 }
 
-private val input: String by lazy { readInput()}
+fun findRoute(puzzle: List<CharArray>, startX: Int, startY: Int): Boolean {
+    val max = puzzle.size
+    val maxSteps = max * max
 
+    var x = startX
+    var y = startY
+    var prevX = startX
+    var prevY = startY
+    var dir = 0
+    var steps = 0
+
+    puzzle[x][y] = 'X'
+    while (x >= 0 && y >= 0 && x < max && y < max && steps < maxSteps) {
+        if (puzzle[x][y] == '#') {
+            dir++
+            dir %= 4
+            x = prevX
+            y = prevY
+        } else {
+            prevX = x
+            prevY = y
+            puzzle[x][y] = 'X'
+            steps++
+        }
+
+        when (dir) {
+            0 -> x--
+            1 -> y++
+            2 -> x++
+            3 -> y--
+        }
+    }
+
+    return steps != maxSteps
+}
+
+@OptIn(ExperimentalForeignApi::class)
 private fun readInput(): String {
-    val returnBuffer = StringBuilder()
     val cwd = getcwd(null, 0U)?.toKString()
     val resourceDir = "${cwd}/src/commonMain/resources"
     val file = fopen("$resourceDir/Day6.input", "r") ?:
     throw IllegalArgumentException("Cannot open input file Day6.input")
+
+    val puzzle: ArrayList<CharArray> = ArrayList()
+    var x: Int = -1
+    var y: Int = -1
+    var indexOf: Int
 
     try {
         memScoped {
@@ -31,13 +73,41 @@ private fun readInput(): String {
             val buffer = allocArray<ByteVar>(readBufferLength)
             var line = fgets(buffer, readBufferLength, file)?.toKString()
             while (line != null) {
-                returnBuffer.append(line)
+                puzzle.add(line.trim().toCharArray())
+                indexOf = line.indexOf("^")
+                if (indexOf > -1) {
+                    x = puzzle.size - 1
+                    y = indexOf
+                }
+
                 line = fgets(buffer, readBufferLength, file)?.toKString()
             }
         }
+
+        //printCharArray(puzzle)
+        //println(startingPoint)
+
+        val originalRoutePuzzle = puzzle.map { it.copyOf() }
+        findRoute(originalRoutePuzzle, x, y)
+
+        var acc = 0
+        val size = originalRoutePuzzle.size
+        for (i in 0..<size) {
+            for (j in 0..<size) {
+                if (originalRoutePuzzle[i][j] == 'X' && !(i == x && j == y)) {
+                    val newPuzzle = puzzle.map { it.copyOf() }
+                    newPuzzle[i][j] = '#'
+                    if (!findRoute(newPuzzle, x, y)) {
+                        acc++
+                    }
+                }
+            }
+        }
+        println(acc)
+
     } finally {
         fclose(file)
     }
 
-    return returnBuffer.toString()
+    return ""
 }
